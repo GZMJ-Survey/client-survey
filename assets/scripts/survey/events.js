85f9874de4c39fabc2f0caf3af25f00d71bcc0b1
skylarkJ <janaryndin@gmail.com>
2017-03-10 15:54:16 -0500

    Add Modal Working On Updating Questions
    Working on API call to update question
    
    By Gabby, Jana, Max, Zhu

--- assets/scripts/survey/events.js
+++ assets/scripts/survey/events.js
@@ -39,33 +39,33 @@ const onUpdateSurveyQuestion = function(event) {
 const onAnswer = function(event) {
   event.preventDefault();
   console.log(surveyStore.length);
-  console.log(this);
+
+  console.log('this is the survey problem id', $('.survey-problem-id')[0].value);
+  console.log('this is the survey id', $('.survey-id')[0].value);
+
   let problems = [];
-  // for (let i = 0; i < surveyStore.length; i++) {
-  //   let result = {};
-  //   result = {
-  //     problem; $('this')
-  //   };
-  // }
-  // let data = {
-  //   survey: {
-  //     questions: problems
-      // questions: [{
-      //   problem: $('.survey-problem').text(),
-      //   answers: {
-      //     response: $('input:checked').val()
-      //   }
-      // }]
+  for (let i = 0; i < surveyStore.length; i++) {
+    let result = {};
+    result = {
+     answers: {
+      response: true
+     }
+    };
+    problems.push(result);
 
-    // }
+  }
+  let data = {
+    survey: {
+      questions: problems
+    }
 
+  };
 
-
-    // api.updateAnswer(data, $('.answer-question').data("id"))
-    // .then((response)=>console.log(response))
-    // .catch((error)=>console.error(error))
-    // .then(ui.successAnswer)
-    // .catch(ui.failureAnswer);
+    api.updateAnswer(data, $('.answer-question').data("id"))
+    .then((response)=>console.log(response))
+    .catch((error)=>console.error(error))
+    .then(ui.successAnswer)
+    .catch(ui.failureAnswer);
 };
 
 const onDestroy = function (event) {
