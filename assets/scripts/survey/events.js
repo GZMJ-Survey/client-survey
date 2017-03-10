ae88ebe3d1ba3cf85b82aa319ff0d7b089890537
skylarkJ <janaryndin@gmail.com>
2017-03-10 15:54:16 -0500

    Add Modal Working On Updating Questions
    Working on API call to update question
    
    By Gabby, Jana, Max, Zhu

--- assets/scripts/survey/events.js
+++ assets/scripts/survey/events.js
@@ -50,58 +50,32 @@ const onAnswer = function(event) {
   event.preventDefault();
   console.log(surveyStore.length);
 
-  let answers = [];
-  let result = {
-       answers: {
-        response: false
-       }
-     };
+  console.log('this is the survey problem id', $('.survey-problem-id')[0].value);
+  console.log('this is the survey id', $('.survey-id')[0].value);
 
+  let problems = [];
   for (let i = 0; i < surveyStore.length; i++) {
-    answers[i] = result;
-  }
-  console.log(answers);
+    let result = {};
+    result = {
+     answers: {
+      response: true
+     }
+    };
+    problems.push(result);
 
+  }
   let data = {
     survey: {
-      questions: answers
+      questions: problems
     }
-  };
 
-  console.log(data);
+  };
 
     api.updateAnswer(data, $('.answer-question').data("id"))
     .then((response)=>console.log(response))
     .catch((error)=>console.error(error))
     .then(ui.successAnswer)
     .catch(ui.failureAnswer);
-  console.log(this);
-  let problems = [];
-  // for (let i = 0; i < surveyStore.length; i++) {
-  //   let result = {};
-  //   result = {
-  //     problem; $('this')
-  //   };
-  // }
-  // let data = {
-  //   survey: {
-  //     questions: problems
-      // questions: [{
-      //   problem: $('.survey-problem').text(),
-      //   answers: {
-      //     response: $('input:checked').val()
-      //   }
-      // }]
-
-    // }
-
-
-
-    // api.updateAnswer(data, $('.answer-question').data("id"))
-    // .then((response)=>console.log(response))
-    // .catch((error)=>console.error(error))
-    // .then(ui.successAnswer)
-    // .catch(ui.failureAnswer);
 };
 
 const onDestroy = function (event) {
